3557873507c3304992b0b670b8cd5f00b47c032f
Felipe Santos <santos.felipe13@gmail.com>
2021-06-24 00:32:48 -0300

    Aula 02

---
+++ src/pages/Home.tsx
@@ -0,0 +1,50 @@
+import { useHistory } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImage from '../assets/images/google-icon.svg'
+
+import "../styles/auth.scss"
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
+export function Home () {
+	const history = useHistory();
+	const { user, signInWithGoogle } = useAuth();
+
+	async function signInAndGoToCreateRoom() {
+		if (!user) {
+			await signInWithGoogle();
+		}
+		history.push("/rooms/new")
+	}
+
+	return (
+		<div id="page-auth">
+			<aside>
+				<img src={illustrationImg} alt="Ilustração simbolozando perguntas e respostas" id="home-illustration" />
+				<strong>Crie salas de Q&amp;A ao vivo</strong>
+				<p>Tire as dúvidas da sua audiência em tempo real</p>
+			</aside>
+			<main>
+				<div className="main-content">
+					<img src={logoImg} alt="Logo da aplicação Letmeask" />
+					<button className="create-room" onClick={signInAndGoToCreateRoom}>
+						<img src={googleIconImage} alt="Logo da Google" />
+						Crie sua sala com o Google
+					</button>
+					<div className="separator">Ou entre em uma sala</div>
+					<form>
+						<input 
+							type="text"
+							placeholder="Digite o código da sala" 
+						/>
+						<Button type="submit">
+							Entrar na sala
+						</Button>
+					</form>
+				</div>
+			</main>
+		</div>
+	)
+}
